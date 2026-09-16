66e147ecf03ae022645ef86faa99dc011ad87266
manhasiddiqui <manhasiddiqui19@gmail.com>
2026-09-17 02:55:30 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,22 +2,16 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from config import FEATURE_COLUMNS, TARGET_COLUMN
-
-CATEGORICAL_COLS = [
+categorical_cols = [
     "Season", "Day_Type", "Local_Event", "Room_Type", "Competitor_Demand"
 ]
-NUMERIC_COLS = ["Base_Price"]
-
-
+numeric_cols = ["Base_Price"]
 def create_preprocessor():
     return ColumnTransformer(
         transformers=[
-            ("categorical", OneHotEncoder(handle_unknown="ignore", sparse_output=False), CATEGORICAL_COLS),
-            ("numeric", "passthrough", NUMERIC_COLS),
+            ("categorical", OneHotEncoder(handle_unknown="ignore", sparse_output=False), categorical_cols),
+            ("numeric", "passthrough", numeric_cols),
         ]
     )
-
-
 def split_features_target(data: pd.DataFrame):
-    """Separate model features from the target column."""
     return data[FEATURE_COLUMNS], data[TARGET_COLUMN]
